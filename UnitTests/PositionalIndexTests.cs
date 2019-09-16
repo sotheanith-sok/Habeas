b1a1fca0c70ed2f1ff92d200112127de3867988f
sellabae <devsellabae@gmail.com>
2019-09-15 23:59:57 -0700

    Update unit test code

--- UnitTests/PositionalIndexTests.cs
+++ UnitTests/PositionalIndexTests.cs
@@ -4,6 +4,8 @@ using Search.Document;
 using Search.PositionalInvertedIndexer;
 using Search.Index;
 using Search.Text;
+using System.Runtime.InteropServices;
+using System;
 
 //Aren't they too big to call unit tests?
 
@@ -12,28 +14,34 @@ namespace UnitTests
 {
     public class PositionalIndexTests {
 
+        //Arrange
+        static IDocumentCorpus corpus = DirectoryCorpus.LoadTextDirectory("../../../UnitTests/testCorpus", ".txt");
+        PositionalInvertedIndex index = PositionalInvertedIndexer.IndexCorpus(corpus);
+
         [Theory]
         [MemberData(nameof(Data))]
         public void PositoinalPostingTest(string term, List<PositionalPosting> expected)
         {
-            //Arrange
-            IDocumentCorpus corpus = DirectoryCorpus.LoadTextDirectory("./UnitTests/testCorpus", ".txt");
-            PositionalInvertedIndex index;
-            IList<PositionalPosting> result;
+            var result = index.GetPositionalPostings(term);
 
-            //Act
-            index = IndexCorpus(corpus);
-            result = index.GetPositionalPostings(term);
-            
             //Assert
             Assert.Equal(expected.Count, result.Count);
-            
+            //Assert.Equal(expected, result);       //TODO: Don't know why this does't work..
+
+            //Print the expected and actual
+            Console.WriteLine($"Term:     {term}");
+            Console.Write("Expected: ");
+            expected.ForEach(x => Console.Write($"{x.ToString()}, "));
+            Console.Write("\nResult:   ");
+            ((List<PositionalPosting>)result).ForEach(x => Console.Write($"{x.ToString()}, "));
+            Console.WriteLine();
         }
 
         //Test data for positional inverted index
         //Assumed the terms were processed with BasicTokenProcessor
-        public static TheoryData<string, List<PositionalPosting>> Data =>
-            new TheoryData<string, List<PositionalPosting>> {
+        //The docID in the data is generated depend on different OS
+        public static TheoryData<string, List<PositionalPosting>> Data(){
+            var winData = new TheoryData<string, List<PositionalPosting>> {
                 {"hello", new List<PositionalPosting>{
                     new PositionalPosting(0, new List<int>{0,1}),
                     new PositionalPosting(2, new List<int>{0,2,3})
@@ -42,41 +50,27 @@ namespace UnitTests
                     new PositionalPosting(1, new List<int>{7,8,9}),
                     new PositionalPosting(4, new List<int>{1,2,3})
                 }},
-                {"sun", new List<PositionalPosting>{
-                    new PositionalPosting(3, new List<int>{5})
-                }},
             };
 
-        private static PositionalInvertedIndex IndexCorpus(IDocumentCorpus corpus)
-        {
-            ITokenProcessor processor = new BasicTokenProcessor();
-
-            // Constuct a inverted-index once 
-            PositionalInvertedIndex index = new PositionalInvertedIndex();
-
-            System.Console.WriteLine("UnitTest: Indexing the corpus... with Positional Index");
-            // Index the document
-            foreach (IDocument doc in corpus.GetDocuments())
-            {
-                //Tokenize the documents
-                ITokenStream stream = new EnglishTokenStream(doc.GetContent());
-                IEnumerable<string> tokens = stream.GetTokens();
-
-                foreach (string token in tokens) {
-                    //Process token to term
-                    string term = processor.ProcessToken(token);
-                    //Add term to the index
-                    if(term.Length > 0) {
-                        //TODO: pass a proper position parameter
-                        index.AddTerm(term, doc.DocumentId, 0);
-                    }
-                }
-                stream.Dispose();
+            var macData = new TheoryData<string, List<PositionalPosting>> {
+                {"hello", new List<PositionalPosting>{
+                    new PositionalPosting(2, new List<int>{0,2,3}),
+                    new PositionalPosting(4, new List<int>{0,1}),
+                }},
+                {"snows", new List<PositionalPosting>{
+                    new PositionalPosting(0, new List<int>{1,2,3}),
+                    new PositionalPosting(3, new List<int>{7,8,9}),
+                }},
+            };
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                System.Console.WriteLine("TestData for macOSX");
+                return macData;
+            } else {
+                System.Console.WriteLine("TestData for all others");
+                return winData;
             }
-
-            return index;
         }
-
 
     }
 
