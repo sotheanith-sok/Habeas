aaf312854b0361674fc3847674c68264ce7ef194
Sotheanith Sok <thetheanith@gmail.com>
2019-09-16 00:35:28 -0700

    Add an improve token processor and its unit test.

---
+++ BetterTokenProcessorUnitTest.cs
@@ -0,0 +1,94 @@
+using System;
+using Xunit;
+using Search.Text;
+using System.Collections.Generic;
+
+namespace CECS_529_Search_Engine_Project
+{
+    public class BetterTokenProcessorUnitTest
+    {
+        private BetterTokenProcessor betterTokenProcessor;
+        public BetterTokenProcessorUnitTest()
+        {
+            betterTokenProcessor = new BetterTokenProcessor();
+        }
+
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to remove leading and trailing all non-alphanumerics
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Theory]
+        [InlineData("!@&(*$&Hello.!@#$!@@$#", "Hello")]
+        [InlineData("*!@)*)(*$)(*$192.168.1.1(!)@*)*(*($*)(*!@", "192.168.1.1")]
+        public void TestRemoveNonAlphanumeric(string token, string expected)
+        {
+            Assert.Equal(expected, this.betterTokenProcessor.RemoveNonAlphanumeric(token));
+        }
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to remove all apostrophes 
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Theory]
+        [InlineData("'Y'all'll'nt've'd's", "Yallllntveds")]
+        public void TestRemoveApostrophes(string token, string expected)
+        {
+            Assert.Equal(expected, this.betterTokenProcessor.RemoveApostrophes(token));
+        }
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to remove all quotation marks 
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Theory]
+        [InlineData("cklda\"fsafdki\"jfd", "ckldafsafdkijfd")]
+        public void TestRemoveQuotationMarks(string token, string expected)
+        {
+            Assert.Equal(expected, this.betterTokenProcessor.RemoveQuotationMarks(token));
+        }
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to reformat hyphenated phrases
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Fact]
+        public void TestHyphenateWords()
+        {
+            Assert.Equal(new List<string> { "Hewlett", "Packard", "Computing", "HewlettPackardComputing" }, this.betterTokenProcessor.HyphenateWords("Hewlett-Packard-Computing"));
+        }
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to lower cases all characters
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Theory]
+        [InlineData("QuAntuS TreMoR est FutURUS", "quantus tremor est futurus")]
+        public void TestLowercaseWords(string token, string expected)
+        {
+            Assert.Equal(expected, this.betterTokenProcessor.LowercaseWords(token));
+        }
+
+        /// <summary>
+        /// Test BetterTokenProccesor's ability to generate steam
+        /// </summary>
+        /// <param name="token">Preprocess token</param>
+        /// <param name="expected">Expected postprocess token</param>
+        [Theory]
+        [InlineData("swimming", "swim")]
+        [InlineData("eating", "eat")]
+        [InlineData("loving", "love")]
+
+        public void TestStemWords(string token, string expected)
+        {
+            Assert.Equal(expected, this.betterTokenProcessor.StemWords(token));
+        }
+
+    }
+
+}
