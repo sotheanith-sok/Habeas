f218a806473da6f8266ea620b72f47752b8b0510
Yashua <ovando.yashua@gmail.com>
2019-09-20 17:11:05 -0700

    updates new methods for main askDirectory and measure indexing time

--- src/Program.cs
+++ src/Program.cs
@@ -4,51 +4,93 @@ using Search.PositionalInvertedIndexer;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+
 
 namespace Program
 {
 
-    public class Program
+
+    class Program
     {
+
         public static void Main(string[] args)
         {
+            //requesting for Directory path from user 
+            //IDocumentCorpus corpus = askDirectory();
+
             string _directory = "./corpus";
             IDocumentCorpus corpus = DirectoryCorpus.LoadTextDirectory(_directory);
-            Stopwatch elapsedTime = new Stopwatch();
-            elapsedTime.Start();
-            PositionalInvertedIndex index =  PositionalInvertedIndexer.IndexCorpus(corpus);
-            elapsedTime.Stop();
-            Console.WriteLine("Elapsed = {0}", elapsedTime.Elapsed);
-            string query;
-            IList<Posting> postings;
-
-            while (true)
+   
+            if (corpus != null && corpus.CorpusSize != 0)
             {
-                Console.Write("\nSearch: ");
-                query = Console.ReadLine();
 
-                if (query == ":q")
+
+
+                // implements stopwatch to measure how long it takes to index corpus
+                PositionalInvertedIndex index = positIndex(corpus);
+
+
+                string query;
+                IList<Posting> postings;
+
+                while (true)
                 {
-                    break;
-                }
-                else if (query == ":vocab")
-                {
-                    PositionalInvertedIndexer.PrintVocab(index.GetVocabulary(), 100);
-                }
-                else
-                {
-                    postings = index.GetPostings(query);
-                    foreach (Posting p in postings)
+                    Console.Write("\nSearch: ");
+                    query = Console.ReadLine();
+
+                    if (query == ":q")
                     {
-                        Console.Write($"Document {corpus.GetDocument(p.DocumentId).Title}");
-                        Console.Write($"\t{p.ToString()}");
-                        Console.WriteLine();
+                        break;
                     }
-                    Console.WriteLine($"'{query}' found in {postings.Count} files");
+                    else if (query == ":vocab")
+                    {
+                        PositionalInvertedIndexer.PrintVocab(index.GetVocabulary(), 100);
+                    }
+                    else
+                    {
+                        postings = index.GetPostings(query);
+                        foreach (Posting p in postings)
+                        {
+                            Console.Write($"Document {corpus.GetDocument(p.DocumentId).Title}");
+                            Console.Write($"\t{p.ToString()}");
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine($"'{query}' found in {postings.Count} files");
+                    }
                 }
             }
         }
 
 
+        public static IDocumentCorpus askDirectory()
+        {
+            string _directory;
+            Console.WriteLine("Enter the path of Folder or Directory: ");
+            _directory = Console.ReadLine();
+            if (Directory.Exists(_directory))
+            {
+                return DirectoryCorpus.LoadTextDirectory(_directory);
+            }
+
+            return null;
+
+        }
+
+        public static PositionalInvertedIndex positIndex(IDocumentCorpus corpus)
+        {
+            Stopwatch elapsedTime = new Stopwatch();
+            elapsedTime.Start();
+            PositionalInvertedIndex index = PositionalInvertedIndexer.IndexCorpus(corpus);
+            elapsedTime.Stop();
+            Console.WriteLine("Elapsed = {0}", elapsedTime.Elapsed);
+
+            return index;
+
+        }
+
+
     }
+
+
 }
