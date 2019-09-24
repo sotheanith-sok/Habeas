2ece59277eb8243a01535bcefb9203e84e819fec
sellabae <devsellabae@gmail.com>
2019-09-24 16:11:37 -0700

    Add PhraseMerge() to PhraseLiteral class

--- src/Search.QueryLanguage/PhraseLiteral.cs
+++ src/Search.QueryLanguage/PhraseLiteral.cs
@@ -27,21 +27,88 @@ namespace Cecs429.Search.Query {
 		}
 
 		public IList<Posting> GetPostings(IIndex index) {
-			List<IList<Posting>> TermPostingsList = new List<IList<Posting>>();
+			//A list of posting lists (postings for each term in the phrase)
+			List<IList<Posting>> postingsList = new List<IList<Posting>>();
+
 			//Retrieves the postings for the individual terms in the phrase
-			foreach(string s in mTerms){
-				TermLiteral TL = new TermLiteral(s);
-				IList<Posting> termPostings = TL.GetPostings(index);
-				TermPostingsList.Add(termPostings);
+			foreach(string term in mTerms) {
+				postingsList.Add(index.GetPostings(term));
 			}
-			// and positional merge them together.
-			// throw new NotImplementedException();
+
+			// positional merge them together.
+			//PhraseMerge( 1st, 2nd, 1)
+			//PhraseMerge( (1st+2nd), 3rd, 2)
+
+			// for(int i=1; i<mTerms.Count-1; i++) {
+			// 	IList<Posting> first = index.GetPostings(mTerms[i-1]);
+			// 	IList<Posting> second = index.GetPostings(mTerms[i]);
+			// 	PhraseMerge(first, second, i);
+			// }
+
+			throw new NotImplementedException(); //TODO: return proper value!!
 		}
 
 		public override string ToString() {
 			return "\"" + string.Join(" ", mTerms) + "\"";
 		}
 
-		
+
+		/// <summary>
+		/// Merge posting lists of two terms into one posting list for a phrase literals
+		/// </summary>
+		/// <param name="first">the first list of postings</param>
+		/// <param name="second">the second list of postings</param>
+		/// <param name="gap">the gap between the terms in the phrase. default: 1</param>
+		/// <returns></returns>
+		public IList<Posting> PhraseMerge(IList<Posting> first, IList<Posting> second, int gap = 1) {
+			IList<Posting> newPostingList = new List<Posting>();
+			
+			int i=0;	//track docID in first postings
+			int j=0;	//track docID in second postings
+			//Iterate through all postings in the first and second.
+			while ( (i<=first.Count) && (j<=second.Count) )
+			{
+				//Compare the document IDs
+				if (first[i].DocumentId == second[j].DocumentId)
+				{
+					//then first[i] and second[j] are the candidates.
+					int x=0;	//track the position in i doc
+					int y=0;	//track the position in j doc (check if y is off by 'gap' from x)
+					List<int> pp1 = first[i].Positions;
+					List<int> pp2 = second[j].Positions;
+					List<int> newPositions = new List<int>();
+					//Iterate through all positions in the posting first[i] and second[j]
+					while ( (x<=pp1.Count) && (y<=pp2.Count) )
+					{
+						//Compare the positions
+						int difference = pp1[y] - pp2[x];
+						if ( difference == gap ) {			//y is off by gap from x
+							//Add to new positions
+							newPositions.Add(pp1[x]);
+							x++;
+							y++;
+						} else if ( difference > gap ) {	//y is too far from x
+							x++;
+						} else {							//y comes before x
+							y++;
+						}
+					}
+					//Add to the posting list
+					if(newPositions.Count != 0){
+						newPostingList.Add(new Posting(first[i].DocumentId, newPositions));
+					}
+					i++;
+					j++;
+				}
+				else if ( first[i].DocumentId < second[j].DocumentId ) {
+					i++;
+				}
+				else {
+					j++;
+				}
+			}
+
+			return newPostingList;
+		}
 	}
 }
