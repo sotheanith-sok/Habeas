2ff4a4b27535ab929ff751c5b139fc9886d1cf0d
sellabae <devsellabae@gmail.com>
2019-09-24 18:18:43 -0700

    Add Tests for PositionalMerge

--- UnitTests/PhraseLiteralTests.cs
+++ UnitTests/PhraseLiteralTests.cs
@@ -36,6 +36,47 @@ namespace UnitTests
             
         }
 
+        [Fact]
+        public void PositionalMergeTest_NoResult_ReturnsEmptyPostings(){
+            IList<Posting> first = new List<Posting>{
+                new Posting(1, new List<int>{2,5})
+            };
+            IList<Posting> second = new List<Posting>{
+                new Posting(1, new List<int>{11,20})
+            };
+            IList<Posting> expected = new List<Posting>();
+            
+            //Act
+            IList<Posting> result = (new PhraseLiteral("ttt")).PositionalMerge(first, second, 1);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void PositionalMergeTest_AllRelevant_ReturnsAllMatching(){
+            IList<Posting> first = new List<Posting>{
+                new Posting(1, new List<int>{0,5}),
+                new Posting(2, new List<int>{10,20}),
+                new Posting(4, new List<int>{1040,2800,9999})
+            };
+            IList<Posting> second = new List<Posting>{
+                new Posting(1, new List<int>{1,6}),
+                new Posting(2, new List<int>{11,21}),
+                new Posting(4, new List<int>{1041,2801,10000})
+            };
+            IList<Posting> expected = first;
+            
+            //Act
+            IList<Posting> result = (new PhraseLiteral("ttt")).PositionalMerge(first, second, 1);
+
+            //Assert
+            //TODO: Use Fluent Assertion and edit below!
+            Assert.Equal(expected[0].ToString(), result[0].ToString());
+            Assert.Equal(expected[1].ToString(), result[1].ToString());
+            Assert.Equal(expected[2].ToString(), result[2].ToString());
+        }
+
         // /// <summary>
         // /// generate a posting list from a string
         // /// </summary>
