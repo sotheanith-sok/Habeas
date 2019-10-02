64a6d856cb6bedf3b5bf38e4e85e3e033e397b76
sellabae <devsellabae@gmail.com>
2019-10-02 03:02:41 -0700

    Update test for ParseQuery()

--- UnitTests/ParseQueryTest.cs
+++ UnitTests/ParseQueryTest.cs
@@ -8,7 +8,7 @@ namespace UnitTests
     public class ParseQueryTest
     {
         [Fact]
-        public void ParseTest_SingleQuery() {
+        public void ParseTest_SingleQuery_ReturnsTermLiteral() {
             //Arrange
             BooleanQueryParser parser = new BooleanQueryParser();
             string query = "smoothie";
@@ -21,7 +21,7 @@ namespace UnitTests
         }
 
         [Fact]
-        public void ParseTest_AndQuery() {
+        public void ParseTest_AndQuery_ReturnsAndQuery() {
             //Arrange
             BooleanQueryParser parser = new BooleanQueryParser();
             string query = "kiwi banana";
@@ -38,7 +38,7 @@ namespace UnitTests
         }
 
         [Fact]
-        public void ParseTest_OrQuery() {
+        public void ParseTest_OrQuery_ReturnsOrQuery() {
             //Arrange
             BooleanQueryParser parser = new BooleanQueryParser();
             string query = "kiwi + banana";
@@ -55,21 +55,53 @@ namespace UnitTests
         }
 
         [Fact]
-        public void ParseTest_PhraseQuery() {
+        public void ParseTest_PhraseQuery_ReturnsPhraseLiteral() {
             //Arrange
             BooleanQueryParser parser = new BooleanQueryParser();
-            string query = "\"banana smoothie\"";
-            PhraseLiteral expected = new PhraseLiteral("banana smoothie");
+            string query = "\"ice smoothie\"";
+            PhraseLiteral expected = new PhraseLiteral("ice smoothie");
             //Act
             IQueryComponent actual = parser.ParseQuery(query);
             //Assert
-            actual.Should().BeOfType(typeof(PhraseLiteral));
+            actual.Should().BeOfType(typeof(PhraseLiteral));    //TODO: Parser should be fixed!
             ((PhraseLiteral)actual).Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ParseCompoundQueryTest_OrAndQuery() {
+            //Arrange
+            BooleanQueryParser parser = new BooleanQueryParser();
+            string query = "kiwi + banana mango apple + cherry tomato";
+            OrQuery expected = new OrQuery(
+                    new List<IQueryComponent>{
+                        new TermLiteral("kiwi"),
+                        new AndQuery( new List<IQueryComponent>{
+                            new TermLiteral("banana"),
+                            new TermLiteral("mango"),
+                            new TermLiteral("apple")
+                        }),
+                        new AndQuery( new List<IQueryComponent>{
+                            new TermLiteral("cherry"),
+                            new TermLiteral("tomato")
+                        })
+                    });
+            //Act
+            IQueryComponent actual = parser.ParseQuery(query);
+            //Assert
+            //check or-query
+            actual.Should().BeOfType(typeof(OrQuery));
+            ((OrQuery)actual).Components.Should().HaveCount(3);
+            //check and-query
+            ((OrQuery)actual).Components[0].Should().BeOfType(typeof(TermLiteral));
+            ((OrQuery)actual).Components[1].Should().BeOfType(typeof(AndQuery));
+            ((AndQuery)((OrQuery)actual).Components[1]).Components.Should().HaveCount(3);
+            ((OrQuery)actual).Components[2].Should().BeOfType(typeof(AndQuery));
+            ((AndQuery)((OrQuery)actual).Components[2]).Components.Should().HaveCount(2);
         }
 
         //TODO: near literal
         // [Fact]
-        // public void ParseTest_NearLiteral() {
+        // public void ParseTest_NearQuery_ReturnsNearLiteral() {
         //     //Arrange
         //     BooleanQueryParser parser = new BooleanQueryParser();
         //     string query = "[lemon NEAR/2 orange]";
@@ -83,7 +115,7 @@ namespace UnitTests
 
         //TODO: wildcard literal
         // [Fact]
-        // public void ParseTest_WildcardLiteral() {
+        // public void ParseTest_WildcardQuery_ReturnsWildcardLiteral() {
         //     //Arrange
         //     BooleanQueryParser parser = new BooleanQueryParser();
         //     string query = "colo*r";
@@ -95,29 +127,31 @@ namespace UnitTests
         //     ((WildcardLiteral)actual).Should().BeEquivalentTo(expected);
         // }
 
-        //TODO: compound query
+
+        //TODO: test compound query parsing with everything
         [Fact]
-        public void ParseTest_CompoundQueries() {
+        public void ParseCompoundQueryTest_Everything() {
             //Arrange
             BooleanQueryParser parser = new BooleanQueryParser();
-            string query = "kiwi + banana \"ice smoothie\" party";
-            OrQuery expected = new OrQuery(
-                    new List<IQueryComponent>{
-                        new TermLiteral("kiwi"),
-                        new AndQuery( new List<IQueryComponent>{
-                            new TermLiteral("banana"),
-                            new PhraseLiteral("ice smoothie"),
-                            new TermLiteral("party")
-                        })
-                    }
-                );
+            string query = "str*berry kiwi + banana \"ice smoothie\" party + [lemon NEAR/2 orange]";
+            // OrQuery expected = new OrQuery(
+            //         new List<IQueryComponent>{
+            //             new AndQuery( new List<IQueryComponent>{
+            //                 new WildcardLiteral("str*berry"),
+            //                 new TermLiteral("kiwi")
+            //             }),
+            //             new AndQuery( new List<IQueryComponent>{
+            //                 new TermLiteral("banana"),
+            //                 new PhraseLiteral("ice smoothie"),
+            //                 new TermLiteral("party")
+            //             }),
+            //             new NearLiteral("lemon",2,"orange")
+            //         }
+            //     );
             //Act
             IQueryComponent actual = parser.ParseQuery(query);
             //Assert
-            actual.Should().BeOfType(typeof(OrQuery));
-            //TODO: how can I check this compound query component
-            actual.Should().BeEquivalentTo(expected);
+            //actual.Should()...
         }
-
     }
 }
