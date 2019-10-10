32c295f0bac8c8b2e469b56ab84b68564e2397b0
Jesse Blacklock <jessebblacklock@gmail.com>
2019-10-10 00:04:47 -0700

    Removed Vestigal Code From BackendProgram

--- Models/Search.Index/BackendProgram.cs
+++ Models/Search.Index/BackendProgram.cs
@@ -14,16 +14,17 @@ namespace Search.Index
         private static PositionalInvertedIndex index;
         private static IDocumentCorpus corpus;
 
-        public void GenerateIndex(string check) {
-        
+        public void GenerateIndex(string check)
+        {
+
             BooleanQueryParser parser = new BooleanQueryParser();
-       
+
             ITokenProcessor processor = new StemmingTokenProcesor();
-        
+
             AssemblyName appName = Assembly.GetEntryAssembly().GetName();
 
             string projectName = appName.Name;
-      
+
             string projectVersion = appName.Version.Major.ToString()
                               + '.' + appName.Version.Minor.ToString();
 
@@ -49,7 +50,7 @@ namespace Search.Index
                 foreach (Posting p in postings)
                 {
                     IDocument doc = corpus.GetDocument(p.DocumentId);
-                    results.Add(doc.Title+" (Author: "+doc.Author+")");
+                    results.Add(doc.Title + " (Author: " + doc.Author + ")");
                     results.Add(doc.DocumentId.ToString());
                 }
             }
@@ -76,7 +77,8 @@ namespace Search.Index
             if (postings.Count > 0)
             {
                 results.Add(postings.Count.ToString());
-                foreach (Posting p in postings) {
+                foreach (Posting p in postings)
+                {
                     IDocument doc = corpus.GetDocument(p.DocumentId);
                     results.Add(doc.Title);
                     results.Add(doc.DocumentId.ToString());
@@ -95,7 +97,7 @@ namespace Search.Index
         public string termStemmer(string term)
         {
             string result = new StemmingTokenProcesor().StemWords(term);
-      
+
             return result;
         }
 
@@ -121,122 +123,10 @@ namespace Search.Index
         public bool PathContainsContent(string CandidatePath)
         {
 
-            //corpus = DirectoryCorpus.LoadTextDirectory(CandidatePath);
-
-            //if (corpus == null || corpus.CorpusSize == 0)
-            //{
-            //    return false;
-            //}
-            //else { return true; }
-
             return Directory.GetFiles(CandidatePath).Length != 0;
 
         }
 
-        ///<summary>
-        ///Requests Directory/Folder path from user and creates corpus based off that directory
-        ///</summary>
-        public static IDocumentCorpus GetCorpusByAskingDirectory()
-        {
-            IDocumentCorpus corpus;
-            string directory;
-
-            while (true)
-            {
-                //Habeas Corpus!!! lol
-                Console.WriteLine("Enter the path of the directory you wish to search: ");
-                directory = Console.ReadLine();
-
-                if (!Directory.Exists(directory))
-                {
-                    Console.WriteLine("The directory doesn't exist.");
-                    continue;
-                }
-
-                corpus = DirectoryCorpus.LoadTextDirectory(directory);
-
-                if (corpus == null || corpus.CorpusSize == 0)
-                {
-                    Console.WriteLine("The directory is empty.");
-                    continue;
-                }
-                else
-                {
-                    // when valid corpus is constructed
-                    break;
-                }
-            }
-
-            return corpus;
-        }
-
-        /// <summary>
-        /// Perform special queries that start with ':'
-        /// such as ':q', ':vocab', ':stem', ':index', ':help'
-        /// </summary>
-        /// <param name="specialQuery">a special query to be performed</param>
-        public static void PerformSpecialQueries(string specialQuery)
-        {
-            string info_support = "1. single query             Y\n"
-                                + "2. boolean query            N  space for AND, + for OR\n"
-                                + "3. phrase query             N  \"term1 term2 ...\"\n"
-                                + "4. near query               N  [term1 NEAR/k term2]\n"
-                                + "5. wildcard queryv          N  colo*r\n"
-                                + "6. soundex for author name  N";
-            string info_special = ":q             exit the program\n"
-                                + ":stem [token]  print the stemmed token\n"
-                                + ":index [dir]   index a folder\n"
-                                + ":vocab         print vocabulary of the current corpus\n"
-                                + ":h, :help      help";
-
-            if (!specialQuery.StartsWith(":"))
-            {
-                return;
-            }
-            specialQuery = specialQuery.ToLower();
-
-            if (specialQuery == ":q")
-            {                                             // :q
-                System.Environment.Exit(1); // Exit the console app
-            }
-            else if (specialQuery.StartsWith(":stem "))
-            {                           // :stem
-                string term = specialQuery.Substring(":stem ".Length);
-                Console.WriteLine(new StemmingTokenProcesor().StemWords(term));
-                Console.WriteLine();
-            }
-            else if (specialQuery == ":vocab")
-            {                                    // :vocab
-                PrintVocab(index, 1000);
-            }
-            else if (specialQuery.StartsWith(":index "))
-            {                           // :index
-                string directory = specialQuery.Substring(":index ".Length);
-
-                if (!Directory.Exists(directory))
-                {
-                    Console.WriteLine("The directory doesn't exist.");
-                    return;
-                }
-                if (corpus == null || corpus.CorpusSize == 0)
-                {
-                    Console.WriteLine("The directory is empty.");
-                    return;
-                }
-                corpus = DirectoryCorpus.LoadTextDirectory(directory);
-                index = Indexer.IndexCorpus(corpus);
-            }
-            else if ((specialQuery == ":help") || (specialQuery == ":h"))
-            {        // :help
-                Console.WriteLine("This search engine supports\n" + info_support);
-                Console.WriteLine("\nSpecial queries\n" + info_special);
-            }
-            else
-            {
-                Console.WriteLine("No such special query exist.");
-                Console.WriteLine(info_special);
-            }
-        }
 
         /// <summary>
         /// Prints the sorted vocabulary of an index
@@ -269,10 +159,10 @@ namespace Search.Index
         {
             string finalString;
             int selected = Int32.Parse(doc);
-         
+
             IDocument selectedDocument;
             selectedDocument = corpus.GetDocument(selected);
-          
+
             TextReader content = selectedDocument.GetContent();
             finalString = (content.ReadToEnd());
             content.Dispose();
@@ -288,71 +178,10 @@ namespace Search.Index
             IDocument selectedDocument;
             selectedDocument = corpus.GetDocument(selected);
             finalString = selectedDocument.Title;
-    
+
             return finalString;
         }
 
-        /// <summary>
-        /// Ask the user the document to view
-        /// </summary>
-        /// <param name="postings">posting list to search the selected document from</param>
-        /// <param name="corpus">corpus to get the document content from</param>
-        /// <return>a selected document to view</return>
-        public static IDocument AskDocumentToView(IList<Posting> postings, IDocumentCorpus corpus)
-        {
-            int selected;
-            IDocument selectedDocument;
-            string input;
-
-            //Ask user a document to view
-            while (true)
-            {
-                Console.Write("Select the number to view the document ([Enter] to exit): ");
-                input = Console.ReadLine();
-                //Enter to exit
-                if (input.Equals(""))
-                {
-                    selectedDocument = null;
-                    break;
-                }
-                //Take number or ask again if input is not numeric
-                try
-                {
-                    selected = Int32.Parse(input);
-                }
-                catch
-                {
-                    continue;
-                }
-            
-
-                //Ask again if input number is not in range
-                Boolean isSelectedInRange = (selected > 0) && (selected <= postings.Count);
-                if (isSelectedInRange)
-                {
-                    selectedDocument = corpus.GetDocument(postings[selected - 1].DocumentId);
-                    break;
-                }
-            }
-            return selectedDocument;
-        }
-
-        /// <summary>
-        /// Print the content of a document.
-        /// </summary>
-        /// <param name="doc">document to be printed</param>
-        public static void PrintContent(IDocument doc)
-        {
-            if (doc != null)
-            {
-                Console.WriteLine($"\n{doc.Title.ToUpper()}");
-                TextReader content = doc.GetContent();
-                Console.WriteLine(content.ReadToEnd());
-                content.Dispose();
-                ((IDisposable)doc).Dispose();
-            }
-        }
-        
 
     }
 
