5a07fbc8f72499f720fc48bc0f89eeb7305d5949
Jesse Blacklock <jessebblacklock@gmail.com>
2019-10-15 14:06:59 -0700

    Basis for homework

--- Models/Search.Index/DiskIndexWriter.cs
+++ Models/Search.Index/DiskIndexWriter.cs
@@ -1,4 +1,9 @@
 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Search.Query;
+
 namespace Search.Index
 {
     /// <summary>
@@ -11,8 +16,24 @@ namespace Search.Index
         /// </summary>
         /// <param name="index">the index to write</param>
         /// <param name="filePath">the absolute path to save the index files</param>
-        public void WriteIndex(IIndex index, string filePath) {
+        public void WriteIndex(IIndex index, string filePath)
+        {
+            IReadOnlyList<string> vocabulary = index.GetVocabulary();
+            foreach (string s in vocabulary)
+            {
+                long vocabStart = WriteVocab(s);
+                long postingStart = WritePostings(s);
+                WriteVocabTable(s);
+            }
+        }
 
+        /// <summary>
+        /// Writes the vocab.bin file
+        /// </summary>
+        public long WriteVocab(string vocabulary)
+        {
+
+            return ;
         }
     }
 }
