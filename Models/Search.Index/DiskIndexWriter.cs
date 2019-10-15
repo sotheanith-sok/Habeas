fb699236b0eb10cc77b8f6a388fa20aae886274d
sellabae <devsellabae@gmail.com>
2019-10-15 13:33:46 -0700

    Add new class to write index on disk

---
+++ Models/Search.Index/DiskIndexWriter.cs
@@ -0,0 +1,18 @@
+
+namespace Search.Index
+{
+    /// <summary>
+    /// Write index to the disk
+    /// </summary>
+    public class DiskIndexWriter
+    {
+        /// <summary>
+        /// Writes the index on disk with 3 files
+        /// </summary>
+        /// <param name="index">the index to write</param>
+        /// <param name="filePath">the absolute path to save the index files</param>
+        public void WriteIndex(IIndex index, string filePath) {
+
+        }
+    }
+}
