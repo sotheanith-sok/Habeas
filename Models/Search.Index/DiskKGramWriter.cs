0fc5da715d6b3ef397b51a9cfcdd142d27a9c61b
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-10-21 20:49:33 -0700

    add comment

--- Models/Search.Index/DiskKGramWriter.cs
+++ Models/Search.Index/DiskKGramWriter.cs
@@ -3,9 +3,16 @@ using Search.Document;
 using System.Collections.Generic;
 namespace Search.Index
 {
+    /// <summary>
+    /// Write KGram only disk
+    /// </summary>
     public class DiskKGramWriter
     {
-
+        /// <summary>
+        /// Write KGram map to disk
+        /// </summary>
+        /// <param name="kGram">KGram object</param>
+        /// <param name="path">where should kGram be written to</param>
         public void WriteKGram(KGram kGram, string path)
         {
             //Generate full path
@@ -13,22 +20,53 @@ namespace Search.Index
             this.WriteKGramTableBin(WriteKGramBin(kGram, path), WriteCandidatesBin(kGram, path), path);
         }
 
+        /// <summary>
+        /// Write candidates to bin file
+        /// </summary>
+        /// <param name="kGram">KGram object</param>
+        /// <param name="path">where should candidates bin be written to</param>
+        /// <returns>Starting positions of each candidates</returns>
         private List<long> WriteCandidatesBin(KGram kGram, string path)
         {
             List<long> candidatesPositions = new List<long>();
-            path = Path.Join(path, "KGram.bin");
+            path = Path.Join(path, "candidates.bin");
             using (BinaryWriter b = new BinaryWriter(File.Create(path)))
             {
-                foreach (string key in kGram.GetKGramMap().Keys){
-                    
+                foreach (List<string> candidates in kGram.GetKGramMap().Values)
+                {
+                    //TODO:
                 }
             }
             return candidatesPositions;
         }
 
+        /// <summary>
+        /// Write KGram to bin file
+        /// </summary>
+        /// <param name="kGram">KGram object</param>
+        /// <param name="path">where should KGram bin be written to</param>
+        /// <returns></returns>
         private List<long> WriteKGramBin(KGram kGram, string path)
         {
+            List<long> kGramPositions = new List<long>();
+            path = Path.Join(path, "kgram.bin");
+            using (BinaryWriter b = new BinaryWriter(File.Create(path)))
+            {
+                foreach (string kgram in kGram.GetKGramMap().Keys)
+                {
+                    kGramPositions.Add(b.BaseStream.Position);
+                    b.Write(kgram);
+                }
+            }
+            return kGramPositions;
         }
+
+        /// <summary>
+        /// Write relationship between KGram bin and candidates bin
+        /// </summary>
+        /// <param name="kGramPositions">Starting positions of all kGram</param>
+        /// <param name="CandidatesPositions">Starting position of all candidates</param>
+        /// <param name="path">where should KGramTable bin be written to</param>
         private void WriteKGramTableBin(List<long> kGramPositions, List<long> CandidatesPositions, string path)
         {
 
