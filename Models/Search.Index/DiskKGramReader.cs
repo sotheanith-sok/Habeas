4e22d61bfb8179fdc04ebe641edd31ac410ae901
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-10-29 11:49:32 -0700

    S 65 on disk k gram (#73)
    * Add comments to FileManager
    
    * Skeleton Code
    
    * add comment
    
    * Add mini kgram writer
    
    * Complete KGramOnDiskWriter
    
    * Complete K-Gram Writter
    
    * Need to implement linear search
    
    * Complete on disk index. Might need to change api call
    
    * Add comments and code clean up
    
    * Remove KGram from static class
    
    * Update KGram to remove unneccessary object.

--- Models/Search.Index/DiskKGramReader.cs
+++ Models/Search.Index/DiskKGramReader.cs
@@ -20,8 +20,9 @@ namespace Search.Index
         public List<string> GetCandidates(string kGram, string path)
         {
             path = Path.GetFullPath(path);
-            List<Entry> entries = GenerateEntries(path, "kGramTable.bin");
-            return SearchValuesBin(SearchKeysBin(entries, kGram, path, "kGram.bin"), path, "candidates.bin");
+            long[] table = GenerateTable(path, "kGramTable.bin");
+            List<string> result = SearchValuesBin(table, SearchKeysBin(kGram, table, path, "kGram.bin"), path, "candidates.bin");
+            return result;
         }
 
         /// <summary>
@@ -32,55 +33,12 @@ namespace Search.Index
         /// <returns></returns>
         public List<string> GetPossibleKGram(string kGram, string path)
         {
-
             path = Path.GetFullPath(path);
-            List<Entry> entries = GenerateEntries(path, "miniKGramTable.bin");
-            return SearchValuesBin(SearchKeysBin(entries, kGram, path, "miniKGram.bin"), path, "miniCandidates.bin");
+            long[] table = GenerateTable(path, "miniKGramTable.bin");
+            List<string> result = SearchValuesBin(table, SearchKeysBin(kGram, table, path, "miniKGram.bin"), path, "miniCandidates.bin");
+            return result;
         }
 
-        /// <summary>
-        /// Search through entires of table bin for a given entry
-        /// </summary>
-        /// <param name="entries">Internal representation of table bin</param>
-        /// <param name="kGram">KGram to be search for</param>
-        /// <param name="path">Path to where the KGram files located</param>
-        /// <param name="filename">name keys bin tie to values in table bin</param>
-        /// <returns>An entry for a given KGram</returns>
-        private Entry SearchKeysBin(List<Entry> entries, string kGram, string path, string filename)
-        {
-            string kGramBin = Path.Join(path, filename);
-
-            int midPoint = entries.Count / 2;
-            string key = "";
-            Entry entry = null;
-            using (BinaryReader binaryReader = new BinaryReader(File.Open(kGramBin, FileMode.Open)))
-            {
-                midPoint = entries.Count / 2;
-                entry = entries[midPoint];
-                binaryReader.BaseStream.Position = entry.keyStart;
-                key = Encoding.UTF8.GetString(entry.keyLength != -1 ? binaryReader.ReadBytes(entry.keyLength) : binaryReader.ReadBytes((int)(binaryReader.BaseStream.Length - binaryReader.BaseStream.Position)));
-            }
-
-            Console.WriteLine(key);
-            if (string.Compare(key, kGram) == 0)
-            {
-                return entry;
-            }
-
-            if (entries.Count > 1)
-            {
-                if (midPoint > 0 && string.Compare(key, kGram) == 1)
-                {
-                    return SearchKeysBin(entries.ToList().GetRange(0, midPoint), kGram, path, filename);
-                }
-                else if (midPoint < entries.Count - 1 && string.Compare(key, kGram) == -1)
-                {
-                    return SearchKeysBin(entries.ToList().GetRange(midPoint + 1, entries.Count - midPoint - 1), kGram, path, filename);
-                }
-
-            }
-            return null;
-        }
 
 
         /// <summary>
@@ -89,10 +47,10 @@ namespace Search.Index
         /// <param name="entry">Entry to be search fro</param>
         /// <param name="path">Path to where the KGram files located</param>
         /// <param name="filename">name of values bin tie to table bin</param>
-        /// <returns></returns>
-        private List<string> SearchValuesBin(Entry entry, string path, string filename)
+        /// <returns>List of string for result</returns>
+        private List<string> SearchValuesBin(long[] table, int index, string path, string filename)
         {
-            if (entry == null)
+            if (index == -1)
             {
                 return new List<string>();
             }
@@ -100,10 +58,14 @@ namespace Search.Index
             {
                 string candidatesBin = Path.Join(path, filename);
 
-                using (BinaryReader reader = new BinaryReader(File.Open(candidatesBin, FileMode.Open)))
+
+                using (FileStream file = File.Open(candidatesBin, FileMode.Open))
+                using (BinaryReader reader = new BinaryReader(file))
                 {
-                    reader.BaseStream.Position = entry.valueStart;
-                    string result = Encoding.UTF8.GetString(reader.ReadBytes(entry.valueLength));
+                    long startByte = table[index];
+                    long length = index < table.Length - 2 ? table[index + 2] - table[index] : file.Length - table[index];
+                    reader.BaseStream.Position = startByte;
+                    string result = Encoding.UTF8.GetString(reader.ReadBytes((int)(length)));
                     return result.Split(" ").ToList();
                 }
             }
@@ -112,80 +74,92 @@ namespace Search.Index
 
 
         /// <summary>
-        /// Generate entries based on a given table bin
+        /// Generate table based on a given table bin
         /// </summary>
         /// <param name="path">Path to where the KGram files located</param>
         /// <param name="filename">Name of table bin</param>
-        /// <returns></returns>
-        private List<Entry> GenerateEntries(string path, string filename)
+        /// <returns>array of long</returns>
+        private long[] GenerateTable(string path, string filename)
         {
             string tableBin = Path.Join(path, filename);
-            List<Entry> entries = new List<Entry>();
-            long previousKey = -1, previousValue = -1, currentKey = -1, currentValue = -1;
-            using (BinaryReader binaryReader = new BinaryReader(File.Open(tableBin, FileMode.Open)))
+            List<long> keyTable = new List<long>();
+            try
             {
-                while (binaryReader.BaseStream.Position != binaryReader.BaseStream.Length)
+                using (FileStream file = File.Open(tableBin, FileMode.Open))
+                using (BinaryReader binaryReader = new BinaryReader(file))
                 {
-                    if (currentKey != -1)
+                    while (binaryReader.BaseStream.Position != binaryReader.BaseStream.Length)
                     {
-                        previousKey = currentKey;
-                        previousValue = currentValue;
-                    }
-
-                    currentKey = binaryReader.ReadInt64();
-                    currentValue = binaryReader.ReadInt64();
-
-                    if (previousKey != -1)
-                    {
-                        Entry entry = new Entry();
-                        entry.keyStart = previousKey;
-                        entry.keyLength = (int)(currentKey - previousKey);
-                        entry.valueStart = previousValue;
-                        entry.valueLength = (int)(currentValue - previousValue);
-                        entries.Add(entry);
+                        keyTable.Add(binaryReader.ReadInt64());
+                        keyTable.Add(binaryReader.ReadInt64());
                     }
                 }
+                return keyTable.ToArray();
 
-                //Add the final entry
-                Entry finalEntry = new Entry();
-                finalEntry.keyStart = currentKey;
-                finalEntry.keyLength = -1;
-                finalEntry.valueStart = currentValue;
-                finalEntry.valueLength = -1;
-                entries.Add(finalEntry);
-
-                return entries;
             }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
         }
 
         /// <summary>
-        /// A representation of bytes entry in table bin.
+        ///Performance binary search on the key bin
         /// </summary>
-        private class Entry
+        /// <param name="term">the term to find</param>
+        /// <returns>the byte position of the postings</returns>
+        private int SearchKeysBin(string term, long[] table, string path, string filename)
         {
-            /// <summary>
-            /// Key start byte location
-            /// </summary>
-            /// <value></value>
-            public long keyStart { get; set; }
+            string keyBinPath = Path.Join(path, filename);
 
-            /// <summary>
-            /// Key bytes length
-            /// </summary>
-            /// <value></value>
-            public int keyLength { get; set; }
+            try
+            {
+                using (FileStream file = File.Open(keyBinPath, FileMode.Open))
+                using (BinaryReader binaryReader = new BinaryReader(file))
+                {
+                    int i = 0;
+                    int j = table.Length / 2 - 1;
+                    while (i <= j)
+                    {
+                        int m = (i + j) / 2;
+                        long termStartByte = table[m * 2];
 
-            /// <summary>
-            /// Value start byte location
-            /// </summary>
-            /// <value></value>
-            public long valueStart { get; set; }
+                        long length = m*2 < table.Length - 2 ? table[m * 2 + 2] - table[m * 2] : file.Length - table[m * 2];
+                        binaryReader.BaseStream.Position=termStartByte;
+                        string termFromFile =Encoding.UTF8.GetString(binaryReader.ReadBytes((int)(length)));
 
-            /// <summary>
-            /// Value length byte
-            /// </summary>
-            /// <value></value>
-            public int valueLength { get; set; }
+                        int compareValue = term.CompareTo(termFromFile);
+                        if (compareValue == 0)
+                        {
+                            // found it!
+                            return m * 2 + 1;
+                        }
+                        else if (compareValue < 0)
+                        {
+                            j = m - 1;
+                        }
+                        else
+                        {
+                            i = m + 1;
+                        }
+                    }
+                }
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return -1;
         }
+
     }
+
+
 }
