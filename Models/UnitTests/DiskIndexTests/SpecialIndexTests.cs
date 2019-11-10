1d53fdb3e628c7b10a98e833929cdd46587c902d
Sotheanith Sok <thetheanith@gmail.com>
2019-11-10 01:37:02 -0800

    Add comment explaining whats going on

--- Models/UnitTests/DiskIndexTests/SpecialIndexTests.cs
+++ Models/UnitTests/DiskIndexTests/SpecialIndexTests.cs
@@ -19,14 +19,22 @@ namespace UnitTests.DiskIndexTest
         [Fact]
         public void GetRankedDocumentsTest()
         {
+            //Path to where all the bin file will be write to
             string pathToIndex = Path.Join(corpusDir, "/index/");
+
+            //Let Indexer know where should it writes all bin files
             Indexer.path = pathToIndex;
+
+            //Read corpus
             IDocumentCorpus corpus = DirectoryCorpus.LoadTextDirectory(corpusDir);
 
-
+            //Create directory  to bins folders if it doesn't exist
             Directory.CreateDirectory(Path.Join(corpusDir, "/index/"));
+
+            //Initialize the index. 
             IIndex index = Indexer.IndexCorpus(corpus);
 
+            //The rest of your code...
             List<string> terms= new List<string>();
             terms.Add("hello");
             index.GetRankedDocuments(terms);
