4b9d5526adc6005431e2b55afa1d4d34bff71f82
Sotheanith Sok <thetheanith@gmail.com>
2019-11-11 18:37:51 -0800

    Complete variable byte encoding
    C

--- Models/Search.OnDiskDataStructure/PostingListEncoderDecoder.cs
+++ Models/Search.OnDiskDataStructure/PostingListEncoderDecoder.cs
@@ -16,34 +16,36 @@ namespace Search.OnDiskDataStructure
         /// <returns>bytes array</returns>
         public byte[] Encoding(List<Posting> value)
         {
-            List<byte> byteValue = new List<byte>();
+            List<byte[]> byteList = new List<byte[]>();
 
             //1. Write document frequency
-            byteValue.AddRange(BitConverter.GetBytes(value.Count));
+            byteList.Add(BitConverter.GetBytes(value.Count));
 
             int previousDocID = 0;
             foreach (Posting p in value)
             {
                 //2. Write docID using gap
-                byteValue.AddRange(BitConverter.GetBytes(p.DocumentId - previousDocID)); //4byte integer per docID
+                byteList.Add(BitConverter.GetBytes(p.DocumentId - previousDocID)); //4byte integer per docID
 
                 List<int> positions = p.Positions;
 
                 //3. Write term frequency (# of positions)
-                byteValue.AddRange(BitConverter.GetBytes(positions.Count));              //4byte integer per term frequency
+                byteList.Add(BitConverter.GetBytes(positions.Count));              //4byte integer per term frequency
+
 
                 //4. Write positions using gap
                 int previousPos = 0;
                 foreach (int pos in positions)
                 {
-                    byteValue.AddRange(BitConverter.GetBytes(pos - previousPos));        //4byte integer per position
+                    byteList.Add(BitConverter.GetBytes(pos - previousPos));        //4byte integer per position
                     previousPos = pos;
+
                 }
 
                 previousDocID = p.DocumentId;
             }
 
-            return byteValue.ToArray();
+            return Compressor.Compress(byteList);
         }
 
         /// <summary>
@@ -54,32 +56,39 @@ namespace Search.OnDiskDataStructure
         public List<Posting> Decoding(byte[] value)
         {
             List<Posting> postingList = new List<Posting>();
-            List<Byte> byteList = new List<byte>(value);
+            
+            List<byte[]> byteList = Compressor.Decompress(value);
 
             int previousDocID = 0;
 
-            //Skip 4 to disregard document frequence
-            int i = 4;
+            //Skip 1 to disregard document frequence
+            int i = 1;
 
-            while (i < value.Length)
+            while (i < byteList.Count)
             {
                 List<int> positions = new List<int>();
                 //Get docID 
-                int docID = BitConverter.ToInt32(byteList.GetRange(i, 4).ToArray()) + previousDocID;
-                i += 4;
+                byte[] intStorage = new byte[4];
+                byteList[i].CopyTo(intStorage, 0);
+                int docID = BitConverter.ToInt32(intStorage) + previousDocID;
+                i += 1;
 
                 //Get term frequence
-                int tf = BitConverter.ToInt32(byteList.GetRange(i, 4).ToArray());
-                i += 4;
+                intStorage = new byte[4];
+                byteList[i].CopyTo(intStorage, 0);
+                int tf = BitConverter.ToInt32(intStorage);
+                i += 1;
                 int previousPos = 0;
 
                 while (tf > 0)
                 {
-                    int pos = BitConverter.ToInt32(byteList.GetRange(i, 4).ToArray()) + previousPos;
+                    intStorage = new byte[4];
+                    byteList[i].CopyTo(intStorage, 0);
+                    int pos = BitConverter.ToInt32(intStorage) + previousPos;
                     positions.Add(pos);
                     previousPos = pos;
                     tf -= 1;
-                    i += 4;
+                    i += 1;
                 }
                 previousDocID = docID;
 
