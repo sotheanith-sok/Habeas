10a54c033c03f3538e884b151de89ba2e1fab9c2
Sotheanith Sok <sotheanith@users.noreply.github.com>
2019-11-14 08:23:19 -0800

    Partiale reversion of on disk dictionary (#86)
    * Revert OnDiskDictionary and reorder methods
    
    * Patch Notes: 11/13/19
    
    .Remove compressor and its tests
    .Update VariableBytes to support compress and decompression of List<bytes[]> and List<int>
    .Update VariableBytes test to support new changes
    .Change Encode and Decode method to support byte array instead of byte
    
    * Update Array.Copy to Buffer.BlockCopy

--- Models/UnitTests/OnDiskDataStructureTests/VariableBytesTests.cs
+++ Models/UnitTests/OnDiskDataStructureTests/VariableBytesTests.cs
@@ -23,7 +23,7 @@ namespace UnitTests.OnDiskDataStructureTests
         [Fact]
         public void EncodeTest_IntList()
         {
-            var actual = VariableBytes.Encode(new List<int>(){128, 0, 255});
+            var actual = VariableBytes.Compress(new List<int>(){128, 0, 255});
             var expected = new byte[]{0x01, 0x80, 0x80, 0x01, 0xFF}; //128, 0, 255
             actual.Should().BeEquivalentTo(expected);
         }
@@ -36,32 +36,7 @@ namespace UnitTests.OnDiskDataStructureTests
             actual.Should().Be(expected);
         }
 
-        [Fact]
-        public void EncodedByteStreamTest()
-        {
-            //Arrange
-            byte[] value = new byte[]{0b00000001, 0b10000000, 0b10000000, 0b00000001, 0b10000001, 0b10001000}; //128, 0, 129, 8
-            var encodedBytes = new VariableBytes.EncodedByteStream(value);
-
-            //Act&Assert
-            
-            //Test Extract()
-            var actual = encodedBytes.Extract();
-            var expected = new byte[]{0b00000001, 0b10000000};
-
-            actual.Should().BeEquivalentTo(expected);
-            encodedBytes.Pos.Should().Be(2);
-
-            //Test Skip()
-            encodedBytes.Skip();
-            encodedBytes.Pos.Should().Be(3);
-
-            //Test ReadInt()
-            var actualNum = encodedBytes.ReadDecodedInt();
-            actualNum.Should().Be(129);
-            encodedBytes.Pos.Should().Be(5);
-            
-        }
+        
         
     }
 }
