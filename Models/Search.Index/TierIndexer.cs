c4a9c9eacd4255e0ebe322be29d2e11119effdbf
Jesse Blacklock <jessebblacklock@gmail.com>
2019-11-25 13:21:08 -0800

    Creates 3 Indices for tiered index

--- Models/Search.Index/TierIndexer.cs
+++ Models/Search.Index/TierIndexer.cs
@@ -12,67 +12,86 @@ namespace Search.Index
 {
     public class TierIndexer
     {
-        //Given an in memory index
-        //Generates the three indexes for the tiered index
-        public static IIndex CreateTiers(DiskPositionalIndex index)
+        //Given an in memory index and a path
+        //Generates the three indices for the tiered index
+        public static void CreateTiers(DiskPositionalIndex index, string path)
         {
             //Collects the vocabulary of the on memory index
             IReadOnlyList<string> vocab = index.GetVocabulary();
-            //creates a priority queue for the purposesu
+ 
+            //creates a priority queue for the purposes
             //of ordering the documents in terms of how frequently the doc contains the term
             MaxPriorityQueue termQueue = new MaxPriorityQueue();
 
-            //What are these lines about? 
-            //Tier1Hashmap
-            //Tier2Hashmap
-            //Tier3Hashmap
-            //Tier 1
-            //Tier 2
-            //Tier 3
+            //declares the diskpositional indices  
+            DiskPositionalIndex Tier1Hashmap = new DiskPositionalIndex(path);
+            DiskPositionalIndex Tier2Hashmap = new DiskPositionalIndex(path);
+            DiskPositionalIndex Tier3Hashmap = new DiskPositionalIndex(path);
+            
+            //hashmap of docIds and postings used for AddTerm function
+            Dictionary<int, List<int>> docIDsAndPostings = new Dictionary<int, List<int>>();
 
             //this foreach loop constructs the 3 indices
             //for each word in the vocab...
             foreach (string v in vocab)
             {
-                //
+                //get postings for the term
                 IList<Posting> postings = index.GetPositionalPostings(v);
-                //
+                
+                //for each posting, get the term frequency and docID
+                //to populate the MaxHeap and docId/posting hashmap
                 foreach (Posting p in postings)
                 {
-                    //
                     int tf = p.Positions.Count;
-                    //
                     int docID = p.DocumentId;
-                    //
+                    docIDsAndPostings.Add(docID,p.Positions);
                     termQueue.MaxHeapInsert(tf, docID);
                 }
-                //
+
+                //create 3 lists of docIDs 
                 List<MaxPriorityQueue.InvertedIndex> Tier1 = termQueue.RetrieveTier(1);
-                //
                 List<MaxPriorityQueue.InvertedIndex> Tier2 = termQueue.RetrieveTier(10);
-                //
                 List<MaxPriorityQueue.InvertedIndex> Tier3 = termQueue.GetPriorityQueue();
-           
-                foreach ()
+
+                //clear the MaxHeap
+                termQueue.ClearHeap();
+
+                foreach (MaxPriorityQueue.InvertedIndex t in Tier1)
                 {
-                    //
-                    foreach (MaxPriorityQueue.InvertedIndex t in Tier1)
-                    {
-                        //
-                        int docID = t.GetDocumentId();
-                        //foreach(int p in positions)
-                        //{
-                        //replace with replace
-                        //Tier1Hashmap.AddTerm(v, docID, p);
-                        //}
+                    int docID = t.GetDocumentId();
+                    List<int> positionList = docIDsAndPostings[docID];
+                    foreach (int p in positionList){
+                    Tier1Hashmap.AddTerm(v, docID, p);
                     }
                 }
-                //
-                termQueue.ClearHeap();
+
+                foreach (MaxPriorityQueue.InvertedIndex t in Tier2)
+                {
+                    int docID = t.GetDocumentId();
+                    List<int> positionList = docIDsAndPostings[docID];
+                    foreach (int p in positionList){
+                    Tier2Hashmap.AddTerm(v, docID, p);
+                    }
+                }
+
+                foreach (MaxPriorityQueue.InvertedIndex t in Tier3)
+                {
+                    int docID = t.GetDocumentId();
+                    List<int> positionList = docIDsAndPostings[docID];
+                    foreach (int p in positionList){
+                    Tier3Hashmap.AddTerm(v, docID, p);
+                    }
+                }
+
+                //clear hashmap of docIDs/postings for later use
+                docIDsAndPostings.Clear();
             }
+
+            //save the indices to the Disk
+            Tier1Hashmap.Save();
+            Tier2Hashmap.Save();
+            Tier3Hashmap.Save();
 
         }
     }
 }
-
-
